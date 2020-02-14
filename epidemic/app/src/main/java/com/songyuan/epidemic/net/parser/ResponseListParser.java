b0a342b1008152f08a7fb9da88174a9b8eeb793d
luoChen <hank.luo@xianghuanji.com>
2020-02-14 20:16:44 +0800

    ..

--- epidemic/app/src/main/java/com/songyuan/epidemic/net/parser/ResponseListParser.java
+++ epidemic/app/src/main/java/com/songyuan/epidemic/net/parser/ResponseListParser.java
@@ -27,7 +27,7 @@ public class ResponseListParser<T> extends AbstractParser<List<T>> {
         final Type type = ParameterizedTypeImpl.get(BaseResponse.class, List.class, mType); //获取泛型类型
         BaseResponse<List<T>> data = convert(response, type);
         List<T> list = data.getAttachment(); //获取data字段
-        if (data.getErrorCode() != 0 || list == null) {  //code不等于0，说明数据不正确，抛出异常
+        if (data.getErrorCode() != 200 || list == null) {  //code不等于0，说明数据不正确，抛出异常
             throw new ParseException(String.valueOf(data.getErrorCode()), data.getMessage(), response);
         }
         return list;
